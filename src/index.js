94209f8cae0689c75fbbfc5cd4c89c8a15c12a8c
Will Burke <billyburke1012@gmail.com>
2021-02-25 15:06:41 -0500

    calculator redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,110 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+//import createStore
+import { createStore } from 'redux';
+
+//setting up default state
+const defaultState = {
+  value: 0
+}
+
+//Action constants and creator functions
+const ADD = 'add';
+const SUBTRACT = 'subtract';
+const MULTIPLY = 'multiply';
+const DIVIDE ='divide';
+
+const add = (amount) => ({
+    type: 'add',
+    payload: {
+      amount
+    }
+
+  }
+);
+
+const subtract = (amount) => ({
+  type: 'subtract',
+  payload: {
+    amount
+  }
+
+}
+);
+
+const multiply = (amount) => ({
+  type: 'multiply',
+  payload: {
+    amount
+  }
+
+}
+);
+
+const divide = (amount) => ({
+  type: 'divide',
+  payload: {
+    amount
+  }
+
+}
+);
+
+// Reducer
+
+const mathTime = (state, action) =>{
+  if (!action) return state;
+  if (!action.payload) return state;
+
+  let newState = {
+    ...state
+  }
+
+  switch (action.type){
+    case ADD:
+      newState.value += action.payload.amount;
+      break;
+    case SUBTRACT:
+      newState.value -= action.payload.amount;
+      break;
+    case MULTIPLY:
+      newState.value *= action.payload.amount;
+      break;
+    case DIVIDE:
+
+      //need conditionals because you cannot divide  0
+      if (action.payload.amount === 0 ){
+        console.log(`can't divide by 0 SILLY GOOSE!!!!!!!!!!!!!`)
+      } else {
+      newState.value /= action.payload.amount;
+      }
+      break;
+    default:
+      break;
+  }
+
+  return newState;
+}
+
+//creating the store
+
+const store = createStore(mathTime, defaultState);
+
+window.store = store
+window.add = add;
+window.subtract = subtract;
+window.multiply = multiply;
+window.divide = divide;
+
+//automatic console
+store.subscribe(() => {
+  console.log('=================UPDATED STATE============');
+  console.table(store.getState())
+
+})
+
+
 
 ReactDOM.render(
   <React.StrictMode>
